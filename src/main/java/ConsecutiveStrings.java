599d2afe78f6243d41b1141f69b17fc1fe47dfeb
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-18 14:12:56 -0500

    added directional comments to method

--- src/main/java/ConsecutiveStrings.java
+++ src/main/java/ConsecutiveStrings.java
@@ -2,7 +2,11 @@ public class ConsecutiveStrings {
 
     //Return the first longest string consisting of k consecutive strings taken in the array.
     public String concatLongest(String[] array, Integer numOfStrings){
+        String firstLongest = "";
+        //concat number of strings together, via for loop
+        //count length and save longest string to variable if greater than length of current saved
+        //return longest string
 
-        return "bob";
+        return firstLongest;
     }
 }
