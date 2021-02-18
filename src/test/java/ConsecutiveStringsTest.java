c3727f8d21e0f2c4f6d453f5861c8407b120010b
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-18 13:54:08 -0500

    setup

---
+++ src/test/java/ConsecutiveStringsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConsecutiveStringsTest {
+    ConsecutiveStrings consecutiveStrings;
+
+    @Before
+    public void setup(){
+        consecutiveStrings = new ConsecutiveStrings();
+
+    }
+
+    @Test
+    public void testMethod(){}
+}
