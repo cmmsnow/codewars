385f159317168b071918d284d160321fa99e85bf
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-18 14:07:51 -0500

    wrote concatStrings test

--- src/main/java/ConsecutiveStrings.java
+++ src/main/java/ConsecutiveStrings.java
@@ -1,2 +1,8 @@
 public class ConsecutiveStrings {
+
+    //Return the first longest string consisting of k consecutive strings taken in the array.
+    public String concatLongest(String[] array, Integer numOfStrings){
+
+        return "bob";
+    }
 }
--- src/test/java/ConsecutiveStringsTest.java
+++ src/test/java/ConsecutiveStringsTest.java
@@ -1,15 +1,21 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ConsecutiveStringsTest {
     ConsecutiveStrings consecutiveStrings;
+    String[] strarr1;
 
     @Before
     public void setup(){
         consecutiveStrings = new ConsecutiveStrings();
-
+        strarr1 = new String[]{"cat", "blob", "manitoba", "lawrenceville"};
     }
 
     @Test
-    public void testMethod(){}
+    public void testConcatLongest(){
+        String expected = "manitobalawrenceville";
+        String actual = consecutiveStrings.concatLongest(strarr1, 2);
+        Assert.assertEquals(expected, actual);
+    }
 }
