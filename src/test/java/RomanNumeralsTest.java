cb02275d9abbc709b9d44d5e8d69765358dec878
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-19 17:10:33 -0500

    all test cases passing but could refactor

--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -8,11 +8,11 @@ public class RomanNumeralsTest {
 
     @Test
     public void shouldConvertToRoman() {
-      //  assertEquals("solution(1) should equal to I", "I", romanNumerals.solution(1));
-      //  assertEquals("solution(4) should equal to IV", "IV", romanNumerals.solution(4));
+        assertEquals("solution(1) should equal to I", "I", romanNumerals.solution(1));
+        assertEquals("solution(4) should equal to IV", "IV", romanNumerals.solution(4));
         assertEquals("solution(6) should equal to VI", "VI", romanNumerals.solution(6));
-      //  assertEquals("solution(10) should equal to X", "X", romanNumerals.solution(10));
-      //  assertEquals("solution(9) should equal to IX", "IX", romanNumerals.solution(9));
+        assertEquals("solution(10) should equal to X", "X", romanNumerals.solution(10));
+        assertEquals("solution(9) should equal to IX", "IX", romanNumerals.solution(9));
         assertEquals("solution(25) should equal to XXV", "XXV", romanNumerals.solution(25));
     }
 }
